a0a761e572750324aa0ec99098b5d57c79529212
yunnian <936321732@qq.com>
2018-05-23 15:09:32 +0800

    example

--- examples/pub.php
+++ examples/pub.php
@@ -1,31 +1,30 @@
 <?php 
 //the nsqd tcp addr that you want to publish
 $nsqdAddr = array(
-    "101.200.220.194:4150",
-//    "127.0.0.1:4154"
+    "127.0.0.1:4150",
+    "127.0.0.1:4154"
 );
 
 $nsq = new Nsq();
 $isTrue = $nsq->connectNsqd($nsqdAddr);
 
-for($i = 0; $i < 101; $i++){
+for($i = 0; $i < 10000; $i++){
     $msg = "nihao".$i;
     $nsq->publish("test", $msg);
 }
 
 $nsq->closeNsqdConnection();
 
-/*
+
 // deferred publish
 $deferred = new Nsq();
 $isTrue = $deferred->connectNsqd($nsqdAddr);
 
 for($i = 0; $i < 9; $i++){
 
-    $delayMsec = 360000;
+    $delayMsec = 3600000;
     //deferredPublish(string topic,string message, int millisecond); 
     $deferred->deferredPublish("test", "the message will be received after".$delayMsec/(1000*60)."minutes", $delayMsec);
 
 }
 $deferred->closeNsqdConnection();
-*/
